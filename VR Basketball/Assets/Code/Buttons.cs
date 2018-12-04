f692cd815595c689aebbf00b6babf6c4aa7fb262
20Dcpenland <20dcpenland@macon.k12.mo.us>
2018-12-04 12:30:24 -0600

    made a lot of sceenes for menus

--- VR Basketball/Assets/Code/Buttons.cs
+++ VR Basketball/Assets/Code/Buttons.cs
@@ -1,16 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //how to get char screen
+    public void toFist1()
+    {
+
+    }
+    public void toClipper2()
+    {
+
+    }
+    public void toSwing3()
+    {
+
+    }
+    public void toSwingDown4()
+    {
+
+    }
+    public void Cross5()
+    {
+
+    }
+    public void Dive6()
+    {
+
+    }
+    public void Trail7()
+    {
+
+    }
+    public void Lift8()
+    {
+
+    }
+    public void Circle9()
+    {
+
+    }
+    public void Rocket10()
+    {
+
+    }
+    public void Euro11()
+    {
+
+    }
+    public void Stay12()
+    {
+
+    }
+    public void Exchange13()
+    {
+
+    }
+    public void Across14()
+    {
+
+    }
+    public void Zipper15()
+    {
+
+    }
+    public void Warrior16()
+    {
+
+    }
+
+    //home page
+    public void GuittGame()
+    {
+        Application.Quit();
+    }
+    public void toInfo()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void toplaySLT1()
+    {
+
+    }
+    public void toplaySLT2()
+    {
+
+    }
+    public void toplaySLT3()
+    {
+
+    } 
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
