5ed962e34587c4c90444d937b7e93a9003ef21ac
Derphitene <Scotty0707@yahoo.com>
2018-12-05 12:09:30 -0600

    Big boy merge

---
+++ VR Basketball/Assets/Player1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player1 : MonoBehaviour
+{
+    NavMeshAgent nm;
+    public Transform[] P1T;
+    public int currentLocation;
+    public int nextLocation;
+    // Use this for initialization
+    void Start()
+    {
+        nm = this.GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, P1T[currentLocation].position);
+        if(/*distance from current location is small and button is clicked*/distance < 2)
+        {
+            Checkpointmove(currentLocation, nextLocation);
+            currentLocation += 1;
+            nm.SetDestination(P1T[currentLocation].position);
+        }
+        
+    }
+
+    private void Checkpointmove(int currentLocation, int nextLocation)
+    {
+        
+        
+
+
+    }
+}
