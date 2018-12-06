7dde8d43eb78747fbcd54129efcfa9ca5c69a69a
Grey Spencer <19grspencer@macon.k12.mo.us>
2018-12-06 14:29:49 -0600

    day 4

--- VR Basketball/Assets/Code/Buttons.cs
+++ VR Basketball/Assets/Code/Buttons.cs
@@ -5,72 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-
-    //how to get char screen
-    public void toFist1()
+    //char sel
+    public void player1()
     {
 
     }
-    public void toClipper2()
+    public void player2()
     {
 
     }
-    public void toSwing3()
+    public void player3()
     {
 
     }
-    public void toSwingDown4()
+    public void player4()
     {
 
     }
-    public void Cross5()
+    public void player5()
     {
 
     }
-    public void Dive6()
-    {
 
-    }
-    public void Trail7()
-    {
 
-    }
-    public void Lift8()
-    {
-
-    }
-    public void Circle9()
-    {
-
-    }
-    public void Rocket10()
-    {
-
-    }
-    public void Euro11()
-    {
-
-    }
-    public void Stay12()
-    {
-
-    }
-    public void Exchange13()
-    {
-
-    }
-    public void Across14()
-    {
-
-    }
-    public void Zipper15()
-    {
-
-    }
-    public void Warrior16()
-    {
-
-    }
 
     //home page
     public void GuittGame()
@@ -79,17 +36,25 @@ public class Buttons : MonoBehaviour
     }
     public void toInfo()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene("Info");
     }
-    public void toplaySLT1()
+    public void toplaySLT1_6()
     {
-
+        SceneManager.LoadScene("Plays1-6");
     }
-    public void toplaySLT2()
+    public void toplaySLT7_12()
     {
-
+        SceneManager.LoadScene("Plays7-12");
     }
-    public void toplaySLT3()
+    public void toplaySLT13_16()
+    {
+        SceneManager.LoadScene("Plays13-16");
+    }
+    public void backtomain()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+    public void toplayerSel()
     {
 
     }
---
+++ VR Basketball/Assets/Code/TheFistScript.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TheFistScript : MonoBehaviour {
+
+
+	public GameObject playerOne;
+	public GameObject playerView;
+
+
+	public Transform[] waypointsPlayer1;
+	public int playerID;
+	protected int actionValue;
+
+	// Use this for initialization
+	void Start () {		
+		if(true /*PlayerNumber == 1*/){
+			playerView.transform.parent = playerOne.transform;
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+	public void NextAction()
+	{
+		actionValue ++;
+		if(actionValue == 1)
+		{
+			// If (PlayerNumber == 1){Shoot(shooter, layup or not, ball position)}
+			//NextMovement(actionValue, waypointsPlayer1, nm1);
+
+		}
+		if(actionValue == 2)
+		{
+			Shoot (1);
+		}
+		if(true)
+		{
+			
+		}
+
+
+
+
+	}
+
+
+	void NextMovement(int stepNumber, Transform[] playerWaypoints, NavMeshAgent nmp)
+	{
+		//nmp.SetDestination (playerWaypoints[stepNumber].transform);
+	}
+
+	void Passing(){
+
+	}
+
+	void Shoot(int player){
+
+	}
+
+	void Block(){
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
